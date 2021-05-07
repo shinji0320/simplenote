a6e9f0fe61cfc180018e487c99d377b5fd317d6c
shinji0320 <shinji.blue.one@gmail.com>
2021-05-07 14:41:08 +0900

    [add]tag

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -24,14 +25,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //ログインしているユーザー情報を渡す
+        $user = \Auth::user();
+        //メモ一覧を取得
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        // dd($memo);
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
     {
         //ログインしているユーザー情報を渡す
         $user = \Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+
+        return view('create', compact('user', 'memos'));
     }
 
     public function store(Request $request)
@@ -40,12 +48,38 @@ class HomeController extends Controller
         // dd($data);
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
+        $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
 
         $memo_id = Memo::insertGetId([
-            'content' => $data['content'], 'user_id' => $data['user_id'], 'status' => 1
+            'content' => $data['content'],
+            'user_id' => $data['user_id'],
+            'tag_id' => $tag_id,
+            'status' => 1
             ]);
 
         // リダイレクト処理
+        return redirect()->route('home');
+    }
+
+    public function edit($id){
+        // 該当するIDのメモをデータベースから取得
+        $user = \Auth::user();
+        $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])
+          ->first();
+        //   dd($memo);
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+
+        $tags = Tag::where('user_id', $user['id'])->get();
+
+        //取得したメモをViewに渡す
+        return view('edit',compact('memo', 'user', 'memos', 'tags'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id'] ]);
         return redirect()->route('home');
     }
 }
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,20 +1,29 @@
+
 @extends('layouts.app')
 
 @section('content')
 <div class="row justify-content-center ml-0 mr-0 h-100">
+    {{ $user['name'] }}
     <div class="card w-100">
-        <div class="card-header">新規メモ作成</div>
+        <div class="card-header">メモ編集</div>
         <div class="card-body">
-            <form method='POST' action="/store">
+            <form method='POST' action="{{ route('update', ['id' => $memo['id']] ) }}">
                 @csrf
                 <input type='hidden' name='user_id' value="{{ $user['id'] }}">
                 <div class="form-group">
-                     <textarea name='content' class="form-control"rows="10"></textarea>
+                     <textarea name='content' class="form-control"rows="10">{{ $memo['content'] }}</textarea>
                 </div>
                 <div class="form-group">
+                    <select class='form-control' name='tag_id'>
+                @foreach($tags as $tag)
+                    <option value="{{ $tag['id'] }}" {{ $tag['id'] == $memo['tag_id'] ? "selected" : "" }}>{{$tag['name']}}</option>
+                @endforeach
+                    </select>
+                </div>
+                <!-- <div class="form-group">
                     <label for="tag">タグ</label>
                     <input name='tag' type="text" class="form-control" id="tag" placeholder="タグを入力">
-                </div>
+                </div> -->
                 <button type='submit' class="btn btn-primary btn-lg">保存</button>
             </form>
         </div>
